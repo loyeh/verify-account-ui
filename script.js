3d1a2506b84e2b3e3e456d908ee9bc9b1d3b58db
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-27 20:03:53 +0330

    bug fixed for mobile

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ digits[0].focus();
 digits.forEach((digit, index) => {
   digit.addEventListener("keyup", (e) => {
     if (e.key != "Backspace") {
-      digit.value = e.key;
       digits[index + 1].focus();
     } else {
       if (digit.value.length == 0) {
